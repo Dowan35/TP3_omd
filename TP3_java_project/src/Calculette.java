4949ea5e26bd361cd03008ba148c81c0777895ce
dowan35 <dowan.gullient@etudiant.univ-rennes1.fr>
2024-11-15 10:28:13 +0100

    lol

--- TP3_java_project/src/Calculette.java
+++ TP3_java_project/src/Calculette.java
@@ -6,10 +6,18 @@ public class Calculette {
     }
 
     public static int soustraction(int a, int b) {
-        return a - b ;
+        return a - b;
+    }
+
+    public static int division(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division par zéro !");
+        }
+        return a / b;
     }
 
     public static void main(String[] args) {
-        System.out.println("1 + 3 = " + addition(1,3));
+        System.out.println("1 + 3 = " + addition(1, 3));
+        System.out.println("Division : " + division(10, 2));
     }
 }
